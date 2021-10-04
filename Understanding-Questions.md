84f4fccb363eca3646d39cb3bcaf9bdc4f6dfbb1
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-10-04 16:02:28 -0400

    created and connected action creator

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,12 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- calls our event handler function
+- event handler dispatches the addOne action
+  *addOne action returns the case type ADD_ONE
+  *reducer case type ADD_ONE returns the current state plus 1
+  ...
+
+- TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import "./App.css";
 import reducer, { initialState } from "./reducers";
 import TotalDisplay from "./components/TotalDisplay";
 import CalcButton from "./components/CalcButton";
-import { addOne } from "./actions/index";
+import { addOne, applyNumber, changeOperator } from "./actions/index";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleAddOneButton = () => {
-    dispatch(addOne());
+  // const handleAddOneButton = () => {
+  //   dispatch(addOne());
+  // };
+  const handleApplyNumber = (number) => {
+    dispatch(applyNumber(number));
+  };
+
+  const handleChangeOperator = (operator) => {
+    dispatch(changeOperator(operator));
   };
 
   return (
@@ -42,27 +49,36 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={() => handleAddOneButton()} value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton onClick={() => handleApplyNumber(1)} value={1} />
+              <CalcButton onClick={() => handleApplyNumber(2)} value={2} />
+              <CalcButton onClick={() => handleApplyNumber(3)} value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton onClick={() => handleApplyNumber(4)} value={4} />
+              <CalcButton onClick={() => handleApplyNumber(5)} value={5} />
+              <CalcButton onClick={() => handleApplyNumber(6)} value={6} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton onClick={() => handleApplyNumber(7)} value={7} />
+              <CalcButton onClick={() => handleApplyNumber(8)} value={8} />
+              <CalcButton onClick={() => handleApplyNumber(9)} value={9} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton
+                onClick={() => handleChangeOperator("+")}
+                value={"+"}
+              />
+              <CalcButton
+                onClick={() => handleChangeOperator("*")}
+                value={"*"}
+              />
+              <CalcButton
+                onClick={() => handleChangeOperator("-")}
+                value={"-"}
+              />
             </div>
 
             <div className="row ce_button">
